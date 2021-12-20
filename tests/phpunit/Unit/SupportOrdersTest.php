6df66ab5a17ac9fdf35b704cfa8b2b51c6e5947e
Devin Price <devin@wptheming.com>
2021-12-20 11:14:43 -0600

    Update tests.

--- tests/phpunit/Unit/SupportOrdersTest.php
+++ tests/phpunit/Unit/SupportOrdersTest.php
@@ -10,20 +10,20 @@ class Replacement_Orders_Test extends WP_UnitTestCase {
 	/**
 	 * Verifies replacement order "created_via" meta.
 	 */
-	public function test_replacement_order_data() {
+	public function test_support_order_data() {
 		$order = WC_Helper_Order::create_order();
-		$replacement = SupportOrders::create_replacement_order( $order );
+		$replacement = SupportOrders::create_support_order( $order );
 		$this->assertEquals( '0.00', $replacement->get_total() );
 	}
 
 	/**
 	 * Verifies is_replacement_order method.
 	 */
-	public function test_is_replacement_order() {
+	public function test_is_support_order() {
 		$order = WC_Helper_Order::create_order();
-		$this->assertFalse( SupportOrders::is_replacement_order( $order ) );
+		$this->assertFalse( SupportOrders::is_support_order( $order ) );
 
-		$replacement = SupportOrders::create_replacement_order( $order );
-		$this->assertTrue( SupportOrders::is_replacement_order( $replacement ) );
+		$replacement = SupportOrders::create_support_order( $order );
+		$this->assertTrue( SupportOrders::is_support_order( $replacement ) );
 	}
 }
